264e9a8d3c75bc13f803a9f531be6bcaa6480cb6
Elyas Rahimi <elyas.rahimi.4.28@gmail.com>
2024-10-30 11:08:03 +0300

    Fixed another blunder in 11.AE1

--- timetable.js
+++ timetable.js
@@ -355,7 +355,7 @@ export const timetables = {
             { subject: "ENGG301", teacher: "AG/MC", room: "W.5", start: "08:45", end: "09:30" },
             { subject: "Break 1", teacher: "N/A", room: "N/A", start: "09:30", end: "09:50" },
             { subject: "ENGG301", teacher: "AG/MC", room: "W.5", start: "09:50", end: "10:35" },
-            { subject: "ENG357", teacher: "W.4", room: "FB/RS", start: "10:35", end: "12:05" },
+            { subject: "ENG357", teacher: "W.4", room: "FB/RS", start: "11:20", end: "12:05" },
             { subject: "Chemistry", teacher: "RY/SS", room: "CH.Lab", start: "12:05", end: "12:45" },
             { subject: "Break 2", teacher: "N/A", room: "N/A", start: "12:45", end: "13:05" },
             { subject: "Chemistry", teacher: "RY/SS", room: "CH.Lab", start: "13:05", end: "13:45" },
